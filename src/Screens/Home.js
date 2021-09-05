cc4849a64df47916f6e86d441c4a83701ffc06b3
Smurf-Hunter <sarwan010300@gmail.com>
2021-09-05 12:33:13 +0530

    Finished Home Screen

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,9 +1,19 @@
 import '../Css/Home.css';
+import {useHistory} from 'react-router-dom'
 
 function Home() {
+  const history = useHistory();
+
+  const sendUser = (address) =>{
+    history.push("/"+address)
+  }
+
   return (
     <div className="Home">
-      I am Home
+      <h2>Welcome to <b>ABKB</b> Bank</h2>
+
+      <button onClick={()=>sendUser("customerLogin")}>Customer Login</button>
+      <button onClick={()=>sendUser("employeeLogin")}>Employee Login</button>
     </div>
   );
 }
